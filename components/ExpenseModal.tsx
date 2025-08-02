7a6b2ba3585d2646810188bb56411aa4cdec1318
rifandyzv <rifandy2@gmail.com>
2025-08-03 00:16:34 +0800

    fix input bar in Ios

--- components/ExpenseModal.tsx
+++ components/ExpenseModal.tsx
@@ -280,6 +280,7 @@ export default function ExpenseModal() {
                   id="amount" 
                   step="0.01" 
                   required 
+                  inputMode="decimal"
                   value={amount}
                   onChange={handleAmountChange}
                 />
